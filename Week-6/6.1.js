06aacc34b1a28ea05bcc5a317c504c26c10e80c5
MohithSuresh <mohithsuresh77@gmail.com>
2023-01-27 17:38:24 +0530

    solved problem 6.1

---
+++ Week-6/6.1.js
@@ -0,0 +1,13 @@
+function maxSubArraySum(a) {
+  var maxint = Math.pow(2, 53);
+  var max_so_far = 0;
+  var max_ending_here = 0;
+
+  for (var i = 0; i < a.length; i++) {
+    max_ending_here = max_ending_here + a[i];
+    if (max_so_far < max_ending_here) max_so_far = max_ending_here;
+
+    if (max_ending_here < 0) max_ending_here = 0;
+  }
+  return max_so_far;
+}
